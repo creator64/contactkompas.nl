31713c0f5a8ed3cea9de1e9efc5ac882db973a3a
Armand <armand.ayar@outlook.com>
2024-08-25 18:59:49 +0200

    chore: load from env file

--- klantenservice_prototype/settings.py
+++ klantenservice_prototype/settings.py
@@ -11,18 +11,18 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3(@st$*kj!p8h-2+05n^6no+i$t7s^%+1wf909f!3zl5jw*1@u'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8_a+-$4w1f^lt3lx1c@d*p4x$ymm_rn7pwb87')
+DEBUG = os.environ.get('DJANGO_DEBUG_VALUE', '') != 'False'
 
 ALLOWED_HOSTS = ['localhost', '192.168.178.21']
 
