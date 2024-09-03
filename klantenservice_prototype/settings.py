92b9457725c0e1fabe829b1f0ea1a5bc345d4d44
Armand <armand.ayar@outlook.com>
2024-09-03 21:08:44 +0200

    chore: cloud only env files are saved in app.yaml

--- klantenservice_prototype/settings.py
+++ klantenservice_prototype/settings.py
@@ -18,10 +18,7 @@ from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-env_path = load_dotenv(os.path.join(BASE_DIR, '.env.local'))
-
-if not env_path:
-    env_path = load_dotenv(os.path.join(BASE_DIR, '.env.cloud'))
+env_path = load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/dev/howto/deployment/checklist/
